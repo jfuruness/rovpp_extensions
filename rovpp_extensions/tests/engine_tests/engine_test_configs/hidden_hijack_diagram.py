3aa408aa8ef8c38e09258cf23d9daf846f550659
Justin Furuness <jfuruness@gmail.com>
2024-06-24 19:15:43 -0400

    hidden hijack test diagram

---
+++ rovpp_extensions/tests/engine_tests/engine_test_configs/hidden_hijack_diagram.py
@@ -0,0 +1,45 @@
+from frozendict import frozendict
+from bgpy.tests.engine_tests.utils import EngineTestConfig
+
+from bgpy.as_graphs.base.links import CustomerProviderLink as CPLink
+from bgpy.as_graphs import ASGraphInfo
+from bgpy.enums import ASNs
+
+from bgpy.simulation_engine import BGP, ROV
+from bgpy.simulation_framework import (
+    ScenarioConfig,
+    SubprefxHijack,
+)
+
+
+as_graph_info = ASGraphInfo(
+    customer_provider_links=frozenset(
+        [
+            CPLink(provider_asn=3, customer_asn=ASNs.ATTACKER.value),
+            CPLink(provider_asn=3, customer_asn=ASNs.VICTIM.value),
+            CPLink(provider_asn=2, customer_asn=3),
+            CPLink(provider_asn=1, customer_asn=2),
+        ]
+    ),
+)
+
+
+desc = ("hidden hijack example against ROV")
+
+hidden_hijack_rov_config = EngineTestConfig(
+    name="hidden_hijack_rov_config",
+    desc=desc,
+    scenario_config=ScenarioConfig(
+        ScenarioCls=SubprefxHijack,
+        BasePolicyCls=BGP,
+        override_attacker_asns=frozenset({ASNs.ATTACKER.value}),
+        override_victim_asns=frozenset({ASNs.VICTIM.value}),
+        override_non_default_asn_cls_dict=frozendict(
+            {
+                2: ROV,
+                ASNs.VICTIM.value: ROV,
+            }
+        ),
+    ),
+    as_graph_info=as_graph_info,
+)
